8be2b271facddb70249c5b07b74d627fe4b158aa
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-10-11 00:44:55 +0100

    Initial Commit

---
+++ GridCalender/GridCalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridCalenderApp.swift
+//  GridCalender
+//
+//  Created by user on 10/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GridCalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GridCalender/ViewModels/Year.swift
@@ -0,0 +1,9 @@
+//
+//  Year.swift
+//  GridCalender
+//
+//  Created by user on 10/10/2023.
+//
+
+import Foundation
+
